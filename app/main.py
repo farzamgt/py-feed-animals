e3ccde53b3ce475e5d63bc5f47f8e7a891b55d62
Farzam <farzamgt@gmail.com>
2025-03-27 09:51:40 +0200

    change parameters.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Animal:
     def __init__(
-            self, name: str, appetite: int, is_hungry: bool = True
+            self, name: str, appetite: int, is_hungry: bool = False
     ) -> None:
         self.name = name
         self.appetite = appetite
